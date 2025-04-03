d690c01b7895d391d7b9208cd5706e87a5a3efec
Princes Joy T. Piaoan <96916344+PiaoanPrincesJoy@users.noreply.github.com>
2025-04-04 01:48:56 +0800

    Update memories.js

--- scripts/memories.js
+++ scripts/memories.js
@@ -2,8 +2,14 @@ function openPopup(imageSrc, description) {
     document.getElementById("popup-img").src = imageSrc;
     document.getElementById("popup-desc").textContent = description;
     document.getElementById("popup").style.display = "flex";
+
+    // Stop background scrolling
+    document.body.classList.add("no-scroll");
 }
 
 function closePopup() {
     document.getElementById("popup").style.display = "none";
+
+    // Re-enable scrolling
+    document.body.classList.remove("no-scroll");
 }
